4aef1300129d9680ab79f7f383f6d0eff85e9023
James-Sutanto <sutantojames@gmail.com>
2024-05-27 17:07:54 +0700

    Edited Home Page

--- company-profile/src/Hero/Hero.js
+++ company-profile/src/Hero/Hero.js
@@ -1,23 +1,33 @@
-import './Hero.css'
-import arrow_btn from "/Users/jamessutanto/CompanyProfile-/company-profile/src/img/arrow_btn.png"
-const Hero = ({heroData, setHeroCount, heroCount}) => {
+import React from 'react';
+import './Hero.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const Hero = ({ heroData, setHeroCount, heroCount }) => {
+  const handlePrevClick = () => {
+    setHeroCount((prevCount) => (prevCount === 0 ? heroData.length - 1 : prevCount - 1));
+  };
+
+  const handleNextClick = () => {
+    setHeroCount((prevCount) => (prevCount === heroData.length - 1 ? 0 : prevCount + 1));
+  };
+
   return (
     <div className='hero'>
       <div className='hero-text'>
-        <p>{heroData.text1}</p>
-        <p>{heroData.text2}</p>
+        <p>{heroData[heroCount].text1}</p>
+        <p>{heroData[heroCount].text2}</p>
       </div>
-     
-      <div className='hero-dot-play'>
-        <ul className='hero-dots'>
-            <li onClick={()=> setHeroCount(0)} className={heroCount===0?"hero-dot orange" : "hero-dot"}></li>
-            <li onClick={()=> setHeroCount(1)} className={heroCount===1?"hero-dot orange" : "hero-dot"}></li>
-            <li onClick={()=> setHeroCount(2)} className={heroCount===2?"hero-dot orange" : "hero-dot"}></li>
-        </ul>
 
+      <div className='hero-arrow-controls'>
+        <button className='hero-arrow' onClick={handlePrevClick}>
+          <i className="fa-solid fa-circle-arrow-left"></i>
+        </button>
+        <button className='hero-arrow' onClick={handleNextClick}>
+          <i className="fa-solid fa-circle-arrow-right"></i>
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- company-profile/src/Home.js
+++ company-profile/src/Home.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import './App.css';
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Background from './Background/Background';
 import Navbar from './Navbar/Navbar';
 import Hero from './Hero/Hero';
-function Home() {
-    const [heroCount, setHeroCount] = useState(0);
 
-    const heroData = [
-      { text1: "Engineering", text2: "Excellence" },
-      { text1: "Designing A ", text2: "Better Tomorrow" },
-      { text1: "We Are Here", text2: "To Serve You" },
-    ];
+function Home() {
+  const [heroCount, setHeroCount] = useState(0);
+
+  const heroData = [
+    { text1: "Engineering", text2: "Excellence" },
+    { text1: "Designing A ", text2: "Better Tomorrow" },
+    { text1: "We Are Here", text2: "To Serve You" },
+  ];
+
   return (
     <div>
       <Background heroCount={heroCount} />
-      <Navbar/>
+      <Navbar />
       <Hero
-        heroData={heroData[heroCount]}
+        heroData={heroData}
         heroCount={heroCount}
         setHeroCount={setHeroCount}
       />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
